288eabfcfd32ba0a96eaa5d17b5c2775f3c428aa
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-08-15 06:38:30 +0500

    Basics of oop revision start

---
+++ 00-Basics/operators/operators.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = 12;
+    // int b = 25; 
+     int a = 10, b = 20;
+    // cout<<"Value of a: "<<a<<endl;
+    // cout<<"Value of a: "<<b<<endl;
+
+    // and operator 
+    // cout<<"Value of a & b: "<<(a & b)<<endl;
+
+ 
+   
+    // int result = !(a < b); 
+    // if the anwer is 1, it will change to 0
+    // cout<<result <<endl; 
+
+    //left shift
+        // cout<<"Value of shift by 3: "<<(a << 3);
+
+    // cout<<"Value of a ^ b: "<<( a ^ b);
+
+    int x=47385;
+    cout<<sizeof(x)<<endl;
+    cout<<sizeof(int)<<endl; 
+   
+    return 0;
+}
---
+++ 00-Basics/operators/stack_heap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+using namespace std;
+int hello(){
+    int a = 100;
+    return a;
+}
+int main(){
+    int a;
+    int b = 20;
+    int *p = &b;
+    int d = hello();
+    cout<<typeid(a).name()<<endl;
+    cout<<b;
+    return 0;
+}
