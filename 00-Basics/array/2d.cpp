37821b28d9ab96e7dfc25bf0eb01fac1d7207692
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-08-16 22:04:55 +0500

    Basic of pf revision added

--- 00-Basics/array/2d.cpp
+++ 00-Basics/array/2d.cpp
@@ -2,39 +2,21 @@
 using namespace std;
 
 int main(){
-    int a[2][3]= {{1,2,3},
-                  {4,5,6}};
-    // cout<<"Elements of Row 1"<<endl;
-    // cout<<a[0][0]<<endl;
-    // cout<<a[0][1]<<endl;
-    // cout<<a[0][2]<<endl;
-    // cout<<"Elements of Row 2"<<endl;
-    // cout<<a[1][0]<<endl;
-    // cout<<a[1][1]<<endl;
-    // cout<<a[1][2]<<endl;
-    
-    // Row 
-    cout<<"Enter the Elements of A[2][3] or 6 Elements: ";
-    for (int i = 0; i < 2; i++)
-    {
-        // Columns
-        for (int j = 0; j <3; j++)
-        {
-            cin>>a[i][j];
-        }
-    }
+ 
 
-    // Display the Array Elements   
-    for (int i = 0; i < 2; i++)
+    int arr[4][5] = {{2,3,4,5},{67,56,45,34,23}} ;
+    // int arr[4][5] = {2,3,4,5,67,56,45,34,23} ;
+    cout<<" Here is a 2d array : "<<endl;
+    for (int i = 0; i <4; i++)
     {
-        // Columns
-        for (int j = 0; j <3; j++)
+        for (int j = 0; j <4; j++)
         {
-            cout<<a[i][j]<<" ";
+            cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
-    
+   
+   
 
     return 0;
 }
